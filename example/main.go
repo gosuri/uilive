2a3783e137468779e9842b7e0be9a4e810106432
Mike Houston <mike@kothar.net>
2016-01-29 17:53:17 +0000

    Added bypass writer which allows data to be written to the underlying output.
    By clearing the uilive.Writer lines and resetting the line count, log output (for example) can
    be written to the underlying output without being overwritten by the next update from the
    buffer.

--- example/main.go
+++ example/main.go
@@ -13,9 +13,12 @@ func main() {
 	// start listening for updates and render
 	writer.Start()
 
-	for i := 0; i <= 100; i++ {
-		fmt.Fprintf(writer, "Downloading.. (%d/%d) GB\n", i, 100)
-		time.Sleep(time.Millisecond * 5)
+	for _, f := range []string{"Foo.zip", "Bar.iso"} {
+		for i := 0; i <= 50; i++ {
+			fmt.Fprintf(writer, "Downloading %s.. (%d/%d) GB\n", f, i, 50)
+			time.Sleep(time.Millisecond * 25)
+		}
+		fmt.Fprintf(writer.Bypass(), "Downloaded %s\n", f)
 	}
 
 	fmt.Fprintln(writer, "Finished: Downloaded 100GB")
