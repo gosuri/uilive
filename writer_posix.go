e481c8e66f15df6bab3844a4a31ba15fabe95d49
Marcus Brummer <mbrlabs7@gmail.com>
2017-04-20 21:26:53 +0200

    Fixes clearLines for posix systems. See https://github.com/gosuri/uilive/issues/11

--- writer_posix.go
+++ writer_posix.go
@@ -8,7 +8,7 @@ import (
 
 func (w *Writer) clearLines() {
 	for i := 0; i < w.lineCount; i++ {
-		fmt.Fprintf(w.Out, "%c[2K", ESC)     // clear the line
 		fmt.Fprintf(w.Out, "%c[%dA", ESC, 1) // move the cursor up
+		fmt.Fprintf(w.Out, "%c[2K", ESC)     // clear the line
 	}
 }
