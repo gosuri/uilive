eafd45c022863b0ad01a61a000d0b19ea60d80a6
Vasily Litvinov <vasilij.n.litvinov@intel.com>
2019-11-07 14:48:30 +0300

    Add forgotten Close() for opened console output

--- terminal_size_windows.go
+++ terminal_size_windows.go
@@ -12,6 +12,7 @@ func getTermSize() (int, int) {
 	if err != nil {
 		return 0, 0
 	}
+	defer out.Close()
 
 	var csbi consoleScreenBufferInfo
 	ret, _, _ := procGetConsoleScreenBufferInfo.Call(out.Fd(), uintptr(unsafe.Pointer(&csbi)))
