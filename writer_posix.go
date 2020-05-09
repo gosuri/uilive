62938cf4f02153274ec8c0b51eee08bc2ec54615
infixint943 <45997896+infixint943@users.noreply.github.com>
2020-05-09 13:47:44 +0530

    Add ability to disable escape sequences
    Useful when I might need to output progress reports to a file, so I won't be needing the escape sequence.

--- writer_posix.go
+++ writer_posix.go
@@ -8,8 +8,8 @@ import (
 )
 
 // clear the line and move the cursor up
-var clear = fmt.Sprintf("%c[%dA%c[2K", ESC, 1, ESC)
+var Clear = fmt.Sprintf("%c[%dA%c[2K", ESC, 1, ESC)
 
 func (w *Writer) clearLines() {
-	_, _ = fmt.Fprint(w.Out, strings.Repeat(clear, w.lineCount))
+	_, _ = fmt.Fprint(w.Out, strings.Repeat(Clear, w.lineCount))
 }
